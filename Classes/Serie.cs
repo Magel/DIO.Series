102dfb5b5c40b8f78ff69939803341bfe9eee2bb
Geraldo Magela Fraga do Nascimento <geraldo@magel.com.br>
2021-05-07 23:01:29 -0300

    finalizei a classe Serie

--- Classes/Serie.cs
+++ Classes/Serie.cs
@@ -1,3 +1,4 @@
+using System; 
 namespace DIO.Series
 {
     public class Serie : EntidadeBase
@@ -5,7 +6,7 @@ namespace DIO.Series
         //Atributos
             private Genero Genero { get; set; }
             private string Titulo { get; set; }
-            private string Descrica { get; set; }
+            private string Descricao { get; set; }
             private int Ano { get; set; }
             private bool Excluido { get; set; }
 
@@ -14,8 +15,38 @@ namespace DIO.Series
             {
                 this.Id = id;
                 this.Genero = genero;
+                this.Titulo = titulo;
+                this.Descricao = descricao;
+                this.Ano = ano;
+                this.Excluido = false;
             }
+        
+        public override string ToString()
+        {
+            string retorno = "";
+            retorno += "Gênero: "           + this.Genero + Environment.NewLine;
+            retorno += "Titulo: "           + this.Titulo + Environment.NewLine;
+            retorno += "Descrição: "        + this.Descricao + Environment.NewLine;
+            retorno += "Ano de Início: "    + this.Ano + Environment.NewLine;
+            retorno += "Excluido: "         + this.Excluido;
+            return retorno;
+        }
 
+        public string retornaTitulo(){
+            return this.Titulo;
+        }
+
+        public int retornaId(){
+            return this.Id;
+        }
+
+        public bool retornaExcluido(){
+            return this.Excluido;
+        }
+
+        public void Excluir(){
+            this.Excluido = true;
+        }
 
     }
 }
