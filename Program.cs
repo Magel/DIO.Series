531e160e800491f2b64d4b94911fd6449db0c4db
Geraldo Magela Fraga do Nascimento <geraldo@magel.com.br>
2021-05-13 01:05:43 -0300

    criei todos os arquivos e estou escrevendo o código

--- Program.cs
+++ Program.cs
@@ -1,12 +1,67 @@
 ﻿using System;
-
 namespace DIO.Series
 {
     class Program
     {
+        static SerieRepositorio repositorio = new SerieRepositorio();
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string opcaoUsuario = ObterOpcaoUsuario();
+
+            while(opcaoUsuario.ToUpper() != "X"){
+                
+                switch (opcaoUsuario){
+                    case "1":
+                        ListarSeries();
+                        break;
+                }
+            }
+        }
+
+        private static void AtualizarSerie(){
+
+            Console.WriteLine("Digite o id da série: ");
+            int indiceSerie = int.Parse(Console.ReadLine());
+
+            foreach (int i in Enum.GetValues(typeof(Genero))){
+                Console.WriteLine("{0}-{1}", i, Enum.GetName(typeof(Genero), i));
+            }
+
+            Console.Write("Digite o gênero entre as opções acima: ");
+            int entradaGenero = int.Parse(Console.ReadLine());
+
+            Console.Write("Digite o Título da Série: ");
+            string entradaTitulo = Console.ReadLine();
+
+            Console.Write("Digite o Ano de Início da Série: ");
+            int entradaAno = int.Parse(Console.ReadLine());
+
+            Console.Write("Digite a Descrição da Série: ");
+            string entradaDescricao = Console.ReadLine();
+
+            Serie atualizaSerie = new Serie(id: indiceSerie, genero: (Genero)entradaGenero, titulo: entradaTitulo, ano: entradaAno, descricao: entradaDescricao);
+            repositorio.Atualiza(indiceSerie, atualizaSerie);
+
+        }
+        private static string ObterOpcaoUsuario(){
+            Console.WriteLine();
+            Console.WriteLine("DIO Séries a seu dispor!");
+            Console.WriteLine("Informe a opção desejada:");
+
+            Console.WriteLine("1- Lista séries");
+            Console.WriteLine("2- Inserir nova série");
+            Console.WriteLine("3- Atualizar série");
+            Console.WriteLine("4- Excluir série");
+            Console.WriteLine("5- Visualizar série");
+            Console.WriteLine("C- Limpar Tela");
+            Console.WriteLine("X- Sair");
+            Console.WriteLine();
+
+            string opcaoUsuario = Console.ReadLine().ToUpper();
+            Console.WriteLine();
+            return opcaoUsuario;
         }
     }
 }
+
+//https://github.com/elizarp/dio-dotnet-poo-lab-2
